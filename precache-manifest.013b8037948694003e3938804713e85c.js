e0b645dd8092fd1210b601144fb773d76d8da11b
Rijal <andreasallbuqori@gmail.com>
2020-09-18 22:32:53 +0700

    Updates

---
+++ precache-manifest.013b8037948694003e3938804713e85c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7e3e6d2e6f5ada0ce79bcc12f5e28ef",
+    "url": "/robo/index.html"
+  },
+  {
+    "revision": "52180833af04176e11f0",
+    "url": "/robo/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "d9670fcb6e53c1393c68",
+    "url": "/robo/static/css/main.fa8180b2.chunk.css"
+  },
+  {
+    "revision": "52180833af04176e11f0",
+    "url": "/robo/static/js/2.6d60624c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/robo/static/js/2.6d60624c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9670fcb6e53c1393c68",
+    "url": "/robo/static/js/main.5cb121d7.chunk.js"
+  },
+  {
+    "revision": "af839f1de40b3e92ec6c",
+    "url": "/robo/static/js/runtime-main.99a06c5d.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robo/static/media/SEGA.12a3db8b.woff"
+  }
+]);
