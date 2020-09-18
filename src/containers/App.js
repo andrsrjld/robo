888431dd377a86398f76acb092c88a6c2123d8d2
Rijal <andreasallbuqori@gmail.com>
2020-09-18 22:27:46 +0700

    update fetch json

--- src/containers/App.js
+++ src/containers/App.js
@@ -16,7 +16,7 @@ class App extends Component {
   }
 
   componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/users')
+    fetch('https://my-json-server.typicode.com/andrsrjld/user/profile')
       .then(response=> response.json())
       .then(users => {this.setState({ robots: users})});
   }
